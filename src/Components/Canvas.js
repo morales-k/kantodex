f03c0a8a0398d3a87d8d43c96788776ff49ba065
Krista M <morales-k@outlook.com>
2023-02-01 22:41:53 -0500

    Added PokeAPI calls
    Created getPokemon and getPokemonData functions to call API & set state.

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,12 +1,14 @@
 import React, { useRef, useState, useEffect } from 'react'
-import { setupCanvas, draw } from '../ViewModel/CanvasVM';
+import { setupCanvas, getPokemon, draw } from '../ViewModel/CanvasVM';
 
 function Canvas() {
     const [canvasReady, setCanvasReady] = useState(false);
+    const [pokeData, setPokeData] = useState(null);
     const canvas = useRef();
 
     useEffect(() => {
         setupCanvas(canvas, setCanvasReady);
+		getPokemon(setPokeData);
       }, []);
     
       useEffect(() => {
