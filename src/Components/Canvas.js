53862d7e7664a66024e16eb62ffbbaecc070adb8
Krista M <morales-k@outlook.com>
2023-02-02 21:45:27 -0500

    API called if pokemon list isn't in cache
    If cache present then list is used to pull pokemon data from storage.

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -3,19 +3,37 @@ import { setupCanvas, getPokemon, draw } from '../ViewModel/CanvasVM';
 
 function Canvas() {
     const [canvasReady, setCanvasReady] = useState(false);
-    const [pokeData, setPokeData] = useState(null);
+	const [pokemonList, setPokemonList] = useState([]);
+    const [pokeData, setPokeData] = useState([]);
     const canvas = useRef();
 
     useEffect(() => {
-        setupCanvas(canvas, setCanvasReady);
-		getPokemon(setPokeData);
-      }, []);
+		const localPokemonList = JSON.parse(localStorage.getItem("pokemonList"));
+		setupCanvas(canvas, setCanvasReady);
+		
+		if (localPokemonList) {
+			let allPokemonData = [];
+			setPokemonList(localPokemonList);
+
+			localPokemonList.forEach(mon => {
+				const id = mon.url.substring(mon.url, mon.url.length - 1).match(/\d+$/)[0];
+				const pokemon = JSON.parse(localStorage.getItem(`pokeID${id}`));
+				if (pokemon) {
+					allPokemonData.push(pokemon);
+				}
+			});
+
+			setPokeData(allPokemonData);
+		} else {
+			getPokemon();
+		}
+	}, []);
     
-      useEffect(() => {
-        if (canvasReady) {
-          draw(canvas);
-        }
-      }, [canvasReady]);
+	useEffect(() => {
+		if (canvasReady) {
+			draw(canvas);
+		}
+	}, [canvasReady]);
 
     return (
         <canvas ref={canvas} />
